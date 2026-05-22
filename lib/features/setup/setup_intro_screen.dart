3dbe4549873ca2f162694ff40f960c65f0072085
Mohammed <marindodkh@gmail.com>
2026-05-22 05:43:31 +0100

    fix the text

--- lib/features/setup/setup_intro_screen.dart
+++ lib/features/setup/setup_intro_screen.dart
@@ -470,7 +470,7 @@ class _SetupIntroScreenState extends State<SetupIntroScreen>
     return const Column(
       children: [
         Text(
-          'أهلاً بك في قضاء الصلوات',
+          'أهلاً بك في قضاء ',
           textAlign: TextAlign.center,
           textDirection: TextDirection.rtl,
           style: TextStyle(
